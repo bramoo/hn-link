f80f8149c1b56cbd289bd079c9bd7fe9f31ef1e2
Bradley Moorfield <brad.moorfield@gmail.com>
2017-12-15 12:37:13 +1300

    Fix key test

--- hnlink.js
+++ hnlink.js
@@ -1,19 +1,14 @@
 (function() {
-    document.addEventListener("keydown", ev => {
-        if (ev.key = "h") {
-            console.log("hnlink: finding link...");
+    console.info("hnlink loaded");
 
+    document.addEventListener("keydown", ev => {
+        if (ev.key == "h") {
             var entry = document.getElementsByClassName("entryBody")[0];
             var array = Array.from(entry.getElementsByTagName("a"));
             var hnlinks = array.filter(e => e.href.indexOf("news.ycombinator.com/item?id=") >= 0);
 
-            console.log("hnlink found " + hnlinks.length + " links");
-            console.log(hnlinks);
-
-            // hnlinks.forEach(e => e.style.border = "5px solid red");
-
             if (hnlinks.length > 0) {
-                console.log("hnlink: opening " + hnlinks[0].href);
+                console.info("hnlink: opening " + hnlinks[0].href);
                 window.open(hnlinks[0].href);
             }
         }
